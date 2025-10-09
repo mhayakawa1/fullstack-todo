abd125a881748f65497b4b9139588395d3d8d75b
M. Hayakawa <93355988+mhayakawa1@users.noreply.github.com>
2025-10-09 11:41:53 -0700

    Add basic elements, create Task component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from "react";
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="">
+        <div>
+          <button>Signup</button>
+          <button>Login</button>
+        </div>
       </header>
+      <div>
+        <h1>TO DO LIST</h1>
+        <form>
+          <input />
+          <button>+</button>
+        </form>
+      </div>
+      <div>
+        <input placeholder="Search" />
+        <div className="">
+          <button className="">
+            <img src="null" alt="" />
+            <span>Sort By</span>
+          </button>
+          <ul className="">
+            <li>Incomplete</li>
+            <li>Complete</li>
+            <li>Date (Ascending)</li>
+            <li>Date (Descending)</li>
+          </ul>
+        </div>
+      </div>
+      <ul>
+        <li>
+          <input />
+          <button>Status</button>
+          <button>Delete</button>
+        </li>
+      </ul>
     </div>
   );
 }
---
+++ src/components/Task.tsx
@@ -0,0 +1,9 @@
+export default function Task() {
+  return (
+    <li>
+      <input />
+      <button>Status</button>
+      <button>Delete</button>
+    </li>
+  );
+}
