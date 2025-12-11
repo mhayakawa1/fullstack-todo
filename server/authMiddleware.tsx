4c4f497618a57614d6d1db25d697005f0973d1ca
M. Hayakawa <93355988+mhayakawa1@users.noreply.github.com>
2025-12-11 07:22:40 -0700

    Fix env variable bugs

--- server/authMiddleware.tsx
+++ server/authMiddleware.tsx
@@ -1,10 +1,11 @@
 import { Request, NextFunction, Response } from "express";
 import jwt from "jsonwebtoken";
-const secret = "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";
+import "dotenv/config";
+const secret = process.env.CLIENT_SECRET;
 
 function checkAuthorization(req: Request, res: Response, next: NextFunction) {
   const token = req.headers.authorization;
-  if (token) {
+  if (token && secret) {
     const verified = jwt.verify(token.split(" ")[1], secret);
     if (verified) {
       next();
