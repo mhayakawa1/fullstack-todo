8ce84b265542df5d089d84246b06933bf76295d7
M. Hayakawa <93355988+mhayakawa1@users.noreply.github.com>
2026-04-01 19:46:09 -0700

    Update url

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -35,7 +35,7 @@ interface Options {
 
 export default function Dashboard() {
   const today = new Date();
-  const url = `${process.env.NODE_ENV === "production" ? "" : "https://localhost:8080"}/api/`;
+  const url = `${process.env.NODE_ENV === "production" ? "https://fullstack-todo-app-server.onrender.com" : "https://localhost:8080"}/api/`;
 
   const defaultSortValue = useMemo(
     () => ({
