735918049b057a12dc94785cf50c90ce8c8c1748
M. Hayakawa <93355988+mhayakawa1@users.noreply.github.com>
2026-04-17 17:25:16 -0700

    Update popup width

--- src/components/DeletePopup.tsx
+++ src/components/DeletePopup.tsx
@@ -17,7 +17,7 @@ export default function DeletePopup(props: PopupProps) {
   };
   return (
     <div className="fixed w-full h-[100vh] -mt-16 z-10 bg-white/50 box-border flex items-center">
-      <div className="flex flex-col gap-4 w-[40vw] min-w-[275px] h-auto bg-white m-auto rounded-lg text-[#1a45bd] p-4">
+      <div className="flex flex-col gap-4 w-[40vw] min-w-[275px] h-auto bg-white m-auto rounded-lg text-[#1a45bd] p-4 box-border">
         <div>
           <h2 className="m-0">Delete "{popupTitle}"</h2>
           <p className="text-sm">Are you sure you want to delete this item?</p>
