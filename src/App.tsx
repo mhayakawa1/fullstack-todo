3a108bd4a61c8360b01a422e98c7292f4fccc676
M. Hayakawa <93355988+mhayakawa1@users.noreply.github.com>
2026-04-18 19:09:12 -0700

    Update clientId, remove console checks

--- src/App.tsx
+++ src/App.tsx
@@ -13,18 +13,10 @@ import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Error from "./components/Error";
 import { clientId } from "./config";
-//eslint-disable-next-line
-console.log("clientId import: ", clientId);
-//eslint-disable-next-line
-console.log("environment: ", process.env.NODE_ENV);
-//eslint-disable-next-line
-console.log("clientId: ", process.env.CLIENT_ID);
-//eslint-disable-next-line
-console.log("react app clientId: ", process.env.REACT_APP_CLIENT_ID);
 
 function App() {
   return (
-    <GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENT_ID || ""}>
+    <GoogleOAuthProvider clientId={clientId}>
       <UserDataProvider>
         <Router>
           <Header />
--- src/config.ts
+++ src/config.ts
@@ -2,16 +2,4 @@ const isProduction = process.env.NODE_ENV === "production";
 
 export const url = `${isProduction ? "https://fullstack-todo-app-server.onrender.com" : "https://localhost:8080"}/api/`;
 
-function getClientId() {
-  let value = "";
-  const id = process.env.CLIENT_ID;
-  const reactId = process.env.REACT_APP_CLIENT_ID;
-  if (id) {
-    value = id;
-  } else if (reactId) {
-    value = reactId;
-  }
-  return value;
-}
-
-export const clientId = getClientId();
+export const clientId = process.env.REACT_APP_CLIENT_ID || "";
